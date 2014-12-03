0c885524ab3233a134cbffe8c8c49ebb0bcf6fe7
Jessica Forbes <jessica-forbes@uiowa.edu>
2014-12-03 17:58:55 -0600

    ENH: Creates new review xml using an ElementTree
    Elements are added to the root element to create the template for
    the review xml for this particular scan that will be sent to XNAT
    once the review is complete.

--- SlicerQCExtensions/ImageEval/parseXML.py
+++ SlicerQCExtensions/ImageEval/parseXML.py
@@ -34,5 +34,47 @@ class ParseXML():
       if 'type' in attribDict.keys():
         self.questionsList.append(attribDict)
 
+  def createReviewXML(self, ID, project, label):
+    root = et.Element('phd:ImageReview')
+    root.attrib = {'ID': ID, 'project': project, 'label': label,
+                   'xmlns:arc':"http://nrg.wustl.edu/arc", 'xmlns:val':"http://nrg.wustl.edu/val",
+                   'xmlns:pipe':"http://nrg.wustl.edu/pipe", 'xmlns:fs':"http://nrg.wustl.edu/fs",
+                   'xmlns:wrk':"http://nrg.wustl.edu/workflow", 'xmlns:scr':"http://nrg.wustl.edu/scr",
+                   'xmlns:xdat':"http://nrg.wustl.edu/security", 'xmlns:cat':"http://nrg.wustl.edu/catalog",
+                   'xmlns:phd':"http://nrg.wustl.edu/phd", 'xmlns:prov':"http://www.nbirn.net/prov",
+                   'xmlns:xnat':"http://nrg.wustl.edu/xnat", 'xmlns:xnat_a':"http://nrg.wustl.edu/xnat_assessments",
+                   'xmlns:xsi':"http://www.w3.org/2001/XMLSchema-instance",
+                   'xsi:schemaLocation':"http://nrg.wustl.edu/fs " \
+                       "https://xnat.hdni.org/xnat/schemas/fs/fs.xsd " \
+                       "http://nrg.wustl.edu/workflow " \
+                       "https://xnat.hdni.org/xnat/schemas/pipeline/workflow.xsd " \
+                       "http://nrg.wustl.edu/catalog " \
+                       "https://xnat.hdni.org/xnat/schemas/catalog/catalog.xsd " \
+                       "http://nrg.wustl.edu/pipe " \
+                       "https://xnat.hdni.org/xnat/schemas/pipeline/repository.xsd " \
+                       "http://nrg.wustl.edu/scr " \
+                       "https://xnat.hdni.org/xnat/schemas/screening/screeningAssessment.xsd " \
+                       "http://nrg.wustl.edu/arc " \
+                       "https://xnat.hdni.org/xnat/schemas/project/project.xsd " \
+                       "http://nrg.wustl.edu/val " \
+                       "https://xnat.hdni.org/xnat/schemas/validation/protocolValidation.xsd " \
+                       "http://nrg.wustl.edu/xnat " \
+                       "https://xnat.hdni.org/xnat/schemas/xnat/xnat.xsd " \
+                       "http://nrg.wustl.edu/phd " \
+                       "https://xnat.hdni.org/xnat/schemas/phd/phd.xsd " \
+                       "http://nrg.wustl.edu/xnat_assessments " \
+                       "https://xnat.hdni.org/xnat/schemas/assessments/assessments.xsd " \
+                       "http://www.nbirn.net/prov " \
+                       "https://xnat.hdni.org/xnat/schemas/birn/birnprov.xsd " \
+                       "http://nrg.wustl.edu/security " \
+                       "https://xnat.hdni.org/xnat/schemas/security/security.xsd"}
+    xnatDate = et.SubElement(root, 'xnat:date')
+    xnatTime = et.SubElement(root, 'xnat:time')
+    phdSeriesNumber = et.SubElement(root, 'phd:series_number')
+    phdFormDescriptor = et.SubElement(root, 'phd:formdescriptor')
+    for questionDict in self.questionsList:
+      et.SubElement(phdFormDescriptor, 'phd:field', attrib=questionDict)
+    et.dump(root)
+
   def getQuestionsList(self):
     return self.questionsList
