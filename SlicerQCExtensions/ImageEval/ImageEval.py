08fe4fd25ebd582c51ce3cf7b162c3f6f45567fb
Jessica Forbes <jessica-forbes@uiowa.edu>
2015-01-12 13:24:20 -0600

    ENH: Append requests module path to sys.path for import

--- SlicerQCExtensions/ImageEval/ImageEval.py
+++ SlicerQCExtensions/ImageEval/ImageEval.py
@@ -8,6 +8,12 @@ import dataBaseSession
 from datetime import datetime
 import urllib
 
+# append path to site-packages directory containing the requests module
+# since the Slicer binaries do not have requests automatically included
+import sys
+sys.path.append('/Library/Python/2.7/site-packages')
+import requests
+
 #
 # ImageEval
 #
